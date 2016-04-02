c6afd75ca4aa7cbb6d0b9e245aa248502945362b
kuimac <lason123>
2016-04-02 22:30:14 +0900

    next 3.3.4 リファクタリング

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -15,5 +16,4 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
   end
-
 end
